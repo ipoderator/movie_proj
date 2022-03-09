195dd1ce1591f00980809029b5bdb73bbdd45e69
Gleb Churkin <gleb.churkin48@gmail.com>
2022-03-09 22:55:08 +0300

    Добавил вторую страницу

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.urls import reverse 
 
-# Create your models here.
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     badget = models.IntegerField(default=1000000)
+
+
+    def get_url(self):
+        return reverse('movie_detail', args=[self.id])
 
 
     def __str__(self) -> str:
