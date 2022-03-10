78b84e89082813a5c12b362e33eff1d0910c9069
Gleb Churkin <gleb.churkin48@gmail.com>
2022-03-10 13:33:20 +0300

    Применил фильтрацию, добавил django ToolBar

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Movie
+from django.db.models import F,Sum,Max,Min,Count,Avg
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by('name', 'badget')
+    agg = movies.aggregate(Avg('badget'),Max('rating'), Min('year'), Count('id'))
     return render(request, 'movie_app/all_movie.html', {
-      'movies' : movies  
+      'movies' : movies,  
+      'agg': agg,
     })
 
 
