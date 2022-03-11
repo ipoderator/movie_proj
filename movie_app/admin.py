5ea59d2d0fddc8c566143bbc9c1381cee06afa30
Gleb Churkin <gleb.churkin48@gmail.com>
2022-03-11 20:51:35 +0300

    много че добавил

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,65 @@
-from django.contrib import admin
-
+from distutils.log import ERROR
+from locale import currency
+from re import search
+from turtle import title
+from django.contrib import admin,messages
+from .models import Movie
+from django.contrib.auth.models import User
+from django.db.models import QuerySet 
 # Register your models here.
+
+
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name  = 'rating'
+
+
+    def lookups(self, request: str, model_admin: str) -> str:
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Средний'),
+            ('от 60 до 70', 'Высокий'),
+            ('>=80', 'Высочайший'),
+        ]
+
+
+    def queryset(self, request: str, queryset: QuerySet) -> QuerySet:
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year','badget', 'rating_status','currency']
+    list_editable = ['rating', 'year','badget','currency']
+    list_per_page = 10
+    actions = ['set_dollars','set_euro']
+    search_fields = ['name', 'rating']
+    list_filter = ['name','currency', RatingFilter]
+
+
+    @admin.display(ordering='rating')
+    def rating_status(self,mov: Movie):
+        if mov.rating < 50:
+            return 'Зачем это смотреть?'
+        if mov.rating < 70:
+            return 'На разок'
+        if mov.rating <= 85:
+            return 'Зачет'
+        return 'Топчик'
+    
+
+    @admin.action(description='Утсановить валюту в долларах')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+
+    @admin.action(description='Установить валюту в евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EURO)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updated} записей',
+        )
--- movie_app/models.py
+++ movie_app/models.py
@@ -4,10 +4,21 @@ from django.utils.text import slugify
 
 
 class Movie(models.Model):
+    EURO = 'EUR'
+    USD = 'USD'
+    RUB = 'RUB'
+    CURRENCY_CHOICES = [
+        (EURO, 'Euro'),
+        (USD, 'Dollar'),
+        (RUB, 'Rubles'),
+    ]
+
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
-    year = models.IntegerField(null=True)
+    year = models.IntegerField(null=True, blank=True)
     badget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB) 
     slug = models.SlugField(default='', null = False, db_index=True)
 
 
